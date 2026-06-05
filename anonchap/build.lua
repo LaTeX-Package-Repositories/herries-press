b9a7e60328a8dbf09d06af1cce501a850c20a296
David Carlisle <d.p.carlisle@gmail.com>
2026-06-05 21:45:39 +0100

    l3build

---
+++ anonchap/build.lua
@@ -0,0 +1,47 @@
+
+module = "anonchap"
+
+version = "2009/08/03 v1.1a"
+
+checkengines={"pdftex"}
+checkruns = 2
+
+textfiles  ={"README.md"}
+
+packtdszip  = false
+
+maxprintline=10000
+typesetruns = 4
+typesetexe="lualatex"
+
+typesetfiles={"anonchap.tex"}
+sourcefiles={"anonchap.sty"}
+
+-- Upload meta data
+
+announce = {}
+announce["2026-06-05 v1.2"] = [[
+Update to support for headings templates when \DocumentMetadata is used
+https://github.com/latex3/tagging-project/issues/1338
+]]
+
+
+uploadconfig = {
+ pkg = "anonchap",
+ version = version,
+ author = "LaTeX Project",
+ license = "lppl1.3c",
+ summary = [[Provides commands to make \chapter typeset like \section.]],
+ ctanPath = "/macros/latex/contrib/anonchap",
+ repository = "https://github.com/LaTeX-Package-Repositories/herries-press",
+ bugtracker = "https://github.com/LaTeX-Package-Repositories/herries-press/issues",
+ uploader = "LaTeX Project Team",
+ email = "latex-team@latex-project.org",
+ update = true ,
+ announcement = announce[version]
+}
+
+
+
+
+
